8b432908d950655f338145a865dd26dd766b8b26
YulJung <cprpdla15@gmail.com>
2021-10-12 15:37:31 +0900

    first commit

---
+++ src/tewst/test.java
@@ -0,0 +1,18 @@
+package tewst;
+
+import java.util.Scanner;
+
+class test1 {
+	Scanner scan = new Scanner(System.in);
+	String text = scan.next();
+}
+
+public class test {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("input");
+		test1 t = new test1();
+		System.out.println(t.text);
+
+	}
+}
